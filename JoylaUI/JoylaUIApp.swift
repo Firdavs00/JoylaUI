811e6118711dadee5d53082e739cff7d114598e6
Firdavs <firdavsvlog@gmail.com>
2021-10-30 11:37:17 +0500

    Initial Commit

---
+++ JoylaUI/JoylaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoylaUIApp.swift
+//  JoylaUI
+//
+//  Created by User on 30/10/21.
+//
+
+import SwiftUI
+
+@main
+struct JoylaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
